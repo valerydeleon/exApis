83df1b7fe019faaf9eeb2fc4fe249b23bf99bb45
Valeria de Leon <valery.deleon@gmail.com>
2017-05-28 23:43:11 -0500

    modal comentario

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+	$('.modal').modal();
+    });
+
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15, // Creates a dropdown of 15 years to control year
+		onStart: () => {
+			$('.picker').appendTo('body');
+		}
+	});
